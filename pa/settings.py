c86ee5ef8a5cd4825db278c204c35d282c892fde
indisecure <67788369+indisecure@users.noreply.github.com>
2023-08-13 16:58:11 +0530

    ok

--- pa/settings.py
+++ pa/settings.py
@@ -64,12 +64,17 @@ WSGI_APPLICATION = 'pa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME':BASE_DIR/ 'db.sqlite3'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':"verceldb" ,
+        'USER': "default",
+        'PASSWORD': "oRQOwf81kaMq",
+        'HOST': "ep-spring-heart-48000382-pooler.us-east-1.postgres.vercel-storage.com",
+        'PORT': "5432",
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
